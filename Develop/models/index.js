efc247a10712c975438905df20f9dc0f0b89c9e7
nic-d <nicanthonidiaz@gmail.com>
2023-11-17 16:46:10 -0500

    finished model associations

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+// 'Product has a foreign key called category_id, being referenced from the Category table'
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+// any foreign key declaration needed here?
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_products'
+});
 
 module.exports = {
   Product,
